f86c217cafd051d2af71f610fa7a8fd4fdb88c31
Thomas Vakili <touzen74@gmail.com>
2012-07-25 20:40:06 +0200

    Added method saldo. Returns a dictionary of all accounts and their balance.
    WARNING: Extremely hacky.
    
    New dependency: BeautifulSoup
    
    TODO: Change "saldo" to "balance".

--- nordea.py
+++ nordea.py
@@ -1,7 +1,12 @@
 import urllib, urllib2, cookielib
+from BeautifulSoup import BeautifulSoup
 
 LOGIN_URL = 'https://mobil.nordea.se/banking-nordea/nordea-c1/login.html'
 ACC_URL = 'https://mobil.nordea.se/banking-nordea/nordea-c1/accounts.html'
+
+
+SALDO_CLASS = 'bold'
+ACC_SALDO_CLASS = 'twoColumn'
 
 class Connection(object):
     def __init__(self, pers_id, code):
@@ -26,6 +31,20 @@ class Connection(object):
         csrf_index = html.index('csrf') + offset
 
         return html[csrf_index : csrf_index + length]
+
+    @property
+    def saldo(self):
+        html = self._opener.open(ACC_URL).read()
+        soup = BeautifulSoup(html, convertEntities="html")
+        
+        acc_list = soup.findAll(attrs=ACC_SALDO_CLASS)
+        saldo_list = soup.findAll(attrs=SALDO_CLASS)[0::2]
+
+        return dict({'total':saldo_list[0].contents[0]}.items()
+                    + {acc_list[i].contents[0].strip():
+                       saldo_list[i+1].contents[0]
+                       for i in xrange(len(acc_list))}.items()) #Puke?
+
 
 class LoginFailed(Exception):
     def __init__(self, err):
