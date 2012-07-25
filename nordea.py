f2a24a3ad50e54406935ae3f80fbbbab4a6191c0
Thomas Vakili <touzen74@gmail.com>
2012-07-25 21:00:01 +0200

    Internationalized. "Saldo" -> "Balance". Minor aesthetical changes (indention of dict).

--- nordea.py
+++ nordea.py
@@ -4,8 +4,8 @@ from BeautifulSoup import BeautifulSoup
 LOGIN_URL = 'https://mobil.nordea.se/banking-nordea/nordea-c1/login.html'
 ACC_URL = 'https://mobil.nordea.se/banking-nordea/nordea-c1/accounts.html'
 
-SALDO_CLASS = 'bold'
-ACC_SALDO_CLASS = 'twoColumn'
+BALANCE_CLASS = 'bold'
+ACC_BALANCE_CLASS = 'twoColumn'
 
 class Connection(object):
     def __init__(self, pers_id, code):
@@ -16,8 +16,8 @@ class Connection(object):
         csrf = soup.find(attrs={'name':'_csrf_token'}).attrMap['value']
 
         login_data = urllib.urlencode({'_csrf_token':csrf,
-                                    'xyz':str(pers_id),
-                                    'zyx':str(code)})
+                                       'xyz':str(pers_id),
+                                       'zyx':str(code)})
 
         response = self._opener.open(LOGIN_URL, login_data).read()
 
@@ -25,16 +25,16 @@ class Connection(object):
             raise LoginFailed(response)
 
     @property
-    def saldo(self):
+    def balance(self):
         html = self._opener.open(ACC_URL).read()
         soup = BeautifulSoup(html, convertEntities="html")
         
-        acc_list = soup.findAll(attrs=ACC_SALDO_CLASS)
-        saldo_list = soup.findAll(attrs=SALDO_CLASS)[0::2]
+        acc_list = soup.findAll(attrs=ACC_BALANCE_CLASS)
+        balance_list = soup.findAll(attrs=BALANCE_CLASS)[0::2]
 
-        return dict({'total':saldo_list[0].contents[0]}.items()
+        return dict({'total':balance_list[0].contents[0]}.items()
                     + {acc_list[i].contents[0].strip():
-                       saldo_list[i+1].contents[0]
+                       balance_list[i+1].contents[0]
                        for i in xrange(len(acc_list))}.items()) #Puke?
 
 class LoginFailed(Exception):
